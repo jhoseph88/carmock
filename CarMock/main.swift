5a4a75a3e1a66aafbb204ebd8dd91fca5aecf3ac
jhoseph88 <ccbarnes88@gmail.com>
2024-01-16 21:08:26 -0500

    multiple dtcs from 03 command; and get rid of headers

--- CarMock/main.swift
+++ CarMock/main.swift
@@ -77,29 +77,27 @@ class CarMockPeripheral: NSObject, CBPeripheralManagerDelegate, CBPeripheralDele
         // get Mode 01 PID support [41 -> 60]
         "0140\r": [Array("7E8 06 41 40 FE D0 84 00 \r".utf8),  Array("\r>".utf8)],
         // Get the current Vehicle VIN Number from the ECU.
-        "0902\r": [
-            Array("SEAR".utf8),
-            Array("CH".utf8),
-            Array("ING...\r".utf8),
-            Array("7E8 10 14 49 02 01 57 56 57 \r7E8 21 41 41 37 41 4A 31 43 \r7E8 22 57 32 39 30 37 35 33 \r".utf8),
-            Array("\r>".utf8)
-        ],
+        "0902\r": [Array("014\n0: 49 02 01 57 56 57\n1: 41 41 37 41 4A 31 43\n2: 57 32 39 30 37 35 33".utf8)],
         // Get the current Calibration ID from the ECU.
         "0904\r": [Array("7E8 10 13 49 04 01 30 37 4B \r7E8 21 39 30 36 30 35 35 44 \r7E8 22 47 20 34 37 34 30 00 \r".utf8), Array("\r>".utf8)],
         // Get the current ECU Name from the ECU.
+        // TODO - reformat to realistic vin response
         "090A\r": [Array("7E8 10 17 49 0A 01 45 43 4D \r7E8 21 00 2D 45 6E 67 69 6E \r7E8 22 65 43 6F 6E 74 72 6F \r7E8 23 6C 00 00 00 00 00 00 \r".utf8), Array("\r>".utf8)],
         "ATDPN\r": [Array("A6\r\r>".utf8)],
         "ATSP6\r": [Array("OK\r\r>".utf8)],
         "ATSP7\r": [Array("OK\r\r>".utf8), Array(">".utf8)],
         "ATSH710\r": [Array("OK\r\r>".utf8)],
         "ATSH7DF\r": [Array("OK\r\r>".utf8)],
+        "ATH0\r": [Array("OK\r\r>".utf8)],
         "ATCAF0\r": [Array("OK\r\r>".utf8)],
         "ATV1\r": [Array("OK\r\r>".utf8)],
         "ATCM000\r": [Array("OK\r\r>".utf8)],
         "ATSH200\r": [Array("OK\r\r>".utf8)],
         "19C000100003011\r": [Array("NO DATA\r\r>".utf8)],
         // Get stored errors - Just P0455 initially; TODO - show more / randomize
-        "03\r": [Array("7E8 04 43 01 04 55 \r".utf8)]
+        "03\r": [Array("43 02 04 55 06 24\r".utf8)],
+        // Reset DTCs
+        "04\r":[Array("OK\r\r>".utf8)],
     ]
     
     required override init() {
@@ -163,7 +161,7 @@ class CarMockPeripheral: NSObject, CBPeripheralManagerDelegate, CBPeripheralDele
                 
                 for updatedVal in valsToUpdate {
                     // Return each updated value after waiting 1 sec
-                    let seconds = 1.0
+                    let seconds = 0.1
                     DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
                         self.pendingUpdateVal = updatedVal
                         self.peripheralManager.updateValue(Data(bytes: updatedVal, count: updatedVal.count), for: dtcCharacteristic, onSubscribedCentrals: nil)
